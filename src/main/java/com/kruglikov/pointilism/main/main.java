21c4b8385e8926968ebc39070251b1f3b4ecfa12
Aaron Rafael Kruglikov <aaronrk@gmail.com>
2019-12-14 00:28:13 -0800

    Minor formatting tweak.

--- src/main/java/com/kruglikov/pointilism/main/main.java
+++ src/main/java/com/kruglikov/pointilism/main/main.java
@@ -11,7 +11,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
